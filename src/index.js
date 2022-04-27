fa23d68729921a8a9b6d609676909a4bafcfba7d
Igor Bjelica <igorjbjelica@gmail.com>
2022-04-27 20:25:22 +0200

    json-mods 2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { HashRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 // component file
 import TodoContainer from "./functionBased/components/TodoContainer"
 
@@ -9,11 +9,11 @@ import "./functionBased/App.css"
 
 ReactDOM.render(
     <React.StrictMode>
-        <HashRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Routes>
                 <Route path="*" element={<TodoContainer />}></Route>
             </Routes>
-        </HashRouter>
+        </BrowserRouter>
     </React.StrictMode>,
     document.getElementById("root")
 )
